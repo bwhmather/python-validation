fcd681f7e8e617b9ac96bedf2e7722727fd420e8
Ben Mather <bwhmather@bwhmather.com>
2017-08-09 23:45:25 +0100

    Implement `validator` decorator.

--- validation/base.py
+++ validation/base.py
@@ -1,2 +1,24 @@
+import functools
+
+
+_undefined = object()
+
+
 def validator(f):
-    return f
+    @functools.wraps(f)
+    def wrapper(value=_undefined, **kwargs):
+        required = kwargs.pop('required', True)
+
+        def validate(value_):
+            if value_ is None:
+                if required:
+                    raise TypeError()
+                return
+            f(value, **kwargs)
+
+        if value is not _undefined:
+            validate(value, **kwargs)
+        else:
+            return validate
+
+    return wrapper
