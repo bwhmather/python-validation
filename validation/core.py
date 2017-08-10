736ab7de6e014da804fa24d3d17832e1bf26c09a
Ben Mather <bwhmather@bwhmather.com>
2017-08-11 00:41:54 +0100

    de-magic validate_int

--- validation/core.py
+++ validation/core.py
@@ -3,20 +3,15 @@ import six
 from .base import validator
 
 
-@validator
-def validate_int(value, min_value=None, max_value=None):
-    """
-    Validator for integer values.
+_undefined = object()
 
-    :param int value:
-        The number to be validated.
-    :param int min_value:
-        The minimum acceptable value for the number.
-    :param int max_value:
-        The maximum acceptable value for the number.
-    :param bool required:
-        Whether the value can be `None`.  Defaults to `True`.
-    """
+
+def _validate_int(value, min_value=None, max_value=None, required=True):
+    if value is None:
+        if required:
+            raise TypeError("required value is None")
+        return
+
     if not isinstance(value, six.integer_types):
         raise TypeError((
             "expected int, but value is of type {cls!r}"
@@ -31,6 +26,39 @@ def validate_int(value, min_value=None, max_value=None):
         raise ValueError((
             "{value!r} is greater than maximum acceptable {max!r}"
         ).format(value=value, max=max_value))
+
+
+def validate_int(
+    value=_undefined,
+    min_value=None, max_value=None,
+    required=True,
+):
+    """
+    Validator for integer values.
+
+    :param int value:
+        The number to be validated.
+    :param int min_value:
+        The minimum acceptable value for the number.
+    :param int max_value:
+        The maximum acceptable value for the number.
+    :param bool required:
+        Whether the value can be `None`.  Defaults to `True`.
+    """
+    _validate_int(min_value, required=False)
+    _validate_int(max_value, min_value=min_value, required=False)
+
+    def validate(value):
+        _validate_int(
+            value,
+            min_value=min_value, max_value=max_value,
+            required=required,
+        )
+
+    if value is not _undefined:
+        validate(value)
+    else:
+        return validate
 
 
 @validator
