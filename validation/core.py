ef814456e5912b9330194b8c1630dff0d17651d8
Ben Mather <bwhmather@bwhmather.com>
2017-08-10 18:53:12 +0100

    Add implementation of and tests for validate_int

--- validation/core.py
+++ validation/core.py
@@ -1,9 +1,36 @@
+import six
+
 from .base import validator
 
 
 @validator
 def validate_int(value, min_value=None, max_value=None):
-    raise NotImplementedError()
+    """
+    Validator for integer values.
+
+    :param int value:
+        The number to be validated.
+    :param int min_value:
+        The minimum acceptable value for the number.
+    :param int max_value:
+        The maximum acceptable value for the number.
+    :param bool required:
+        Whether the value can be `None`.  Defaults to `True`.
+    """
+    if not isinstance(value, six.integer_types):
+        raise TypeError((
+            "expected int, but value is of type {cls!r}"
+        ).format(cls=value.__class__.__name__))
+
+    if min_value is not None and value < min_value:
+        raise ValueError((
+            "{value!r} is less than minimum acceptable {min!r}"
+        ).format(value=value, min=min_value))
+
+    if max_value is not None and value > max_value:
+        raise ValueError((
+            "{value!r} is greater than maximum acceptable {max!r}"
+        ).format(value=value, max=max_value))
 
 
 @validator
--- validation/tests/test_core.py
+++ validation/tests/test_core.py
@@ -1,8 +1,50 @@
 import unittest
 
+import sys
+
+import six
+
+from validation import validate_int
+
 
 class ValidateIntTestCase(unittest.TestCase):
-    pass
+    def test_valid(self):
+        validate_int(0)
+        validate_int(1)
+
+    def test_long(self):
+        if six.PY3:
+            raise unittest.SkipTest("not relevant in python3")
+
+        validate_int(2 * sys.maxint)  # pylint: disable=no-member
+        validate_int(-2 * sys.maxint)  # pylint: disable=no-member
+
+    def test_float(self):
+        with self.assertRaises(TypeError):
+            validate_int(1.0)
+
+    def test_min(self):
+        validate_int(5, min_value=5)
+
+        with self.assertRaises(ValueError):
+            validate_int(5, min_value=6)
+
+    def test_max(self):
+        validate_int(5, max_value=5)
+
+        with self.assertRaises(ValueError):
+            validate_int(5, max_value=4)
+
+    def test_required(self):
+        validate_int(None, required=False)
+
+        with self.assertRaises(TypeError):
+            validate_int(None)
+
+    def test_closure(self):
+        validator = validate_int(min_value=0)
+        with self.assertRaises(ValueError):
+            validator(-1)
 
 
 class ValidateFloatTestCase(unittest.TestCase):
