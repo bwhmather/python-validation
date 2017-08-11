e6a62779b028e6f45293883db9ce911de8e19dc0
Ben Mather <bwhmather@bwhmather.com>
2017-08-11 12:06:43 +0100

    Add tests for validate_set

--- validation/tests/test_datastructures.py
+++ validation/tests/test_datastructures.py
@@ -2,7 +2,7 @@ import unittest
 
 from validation import (
     validate_int,
-    validate_list,
+    validate_list, validate_set,
 )
 
 
@@ -54,7 +54,46 @@ class ValidateListTestCase(unittest.TestCase):
 
 
 class ValidateSetTestCase(unittest.TestCase):
-    pass
+    def test_empty_is_not_missing(self):
+        validate_set(set())
+
+    def test_non_empty_no_validator(self):
+        validate_set({1, 'string'})
+
+    def test_validator_valid(self):
+        validate_set({1, 2, 3}, validator=validate_int())
+
+    def test_validator_invalid(self):
+        with self.assertRaises(ValueError):
+            validate_set({1, 2, 3, -1}, validator=validate_int(min_value=0))
+
+    def test_validate_list(self):
+        with self.assertRaises(TypeError):
+            validate_set([1], validator=validate_int())
+
+    def test_min_len(self):
+        validate_set({1, 2, 3}, min_length=3)
+
+        with self.assertRaises(ValueError):
+            validate_set({1, 2, 3}, min_length=4)
+
+    def test_max_len(self):
+        validate_set({1, 2, 3}, max_length=3)
+
+        with self.assertRaises(ValueError):
+            validate_set({1, 2, 3, 4}, max_length=3)
+
+    def test_required(self):
+        validate_set(None, required=False)
+
+        with self.assertRaises(TypeError):
+            validate_set(None)
+
+    def test_closure(self):
+        validator = validate_set(max_length=3)
+        validator({1})
+        with self.assertRaises(ValueError):
+            validator({1, 2, 3, 4})
 
 
 class ValidateTupleTestCase(unittest.TestCase):
