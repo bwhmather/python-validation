2ab944d916a27547fd091b36f05b3fabdacaff3e
Ben Mather <bwhmather@bwhmather.com>
2017-08-11 12:39:24 +0100

    Add implementation for validate_dict

--- validation/datastructures.py
+++ validation/datastructures.py
@@ -1,3 +1,5 @@
+import six
+
 from .core import _validate_int, _validate_bool
 
 
@@ -164,12 +166,102 @@ def validate_tuple(
     raise NotImplementedError()
 
 
-def validate_dict(
-    value=_undefined,
-    schema=None, allow_extra=False, default=None,
+def _validate_dict(
+    value,
+    schema=None, allow_extra=False,
     required=True,
 ):
-    raise NotImplementedError()
+    if value is None:
+        if not required:
+            return
+        raise TypeError("required value is None")
+
+    if not isinstance(value, dict):
+        raise TypeError((
+            "Expected dictionary but the type is {cls!r}"
+        ).format(cls=value.__class__.__name__))
+
+    if schema is not None:
+        for key, validator in schema.items():
+            if key not in value:
+                raise ValueError((
+                    "key {key} missing from dictionary {dictionary!r}"
+                ).format(key=key, dictionary=value))
+
+            try:
+                validator(value=value[key])
+            except (TypeError, ValueError) as exc:
+                if type(exc) not in (TypeError, ValueError):
+                    # No safe way to extend the message for subclasses.
+                    raise
+
+                # Check that the exception has been properly constructed.  The
+                # documentation requires that :exception:`TypeError`s and
+                # :exception:`ValueError`s are constructed with a single,
+                # string argument, but this is not enforced anywhere.
+                assert len(exc.args) == 1
+                assert isinstance(exc.args[0], str)
+
+                message = "invalid value for key {key!r}: {message}".format(
+                    key=key, message=exc.args[0],
+                )
+
+                six.raise_from(type(exc)(message), exc)
+
+        if not allow_extra and set(schema) != set(value):
+            raise ValueError((
+                "encountered unexpected keys: {unexpected!r}"
+            ).format(unexpected=set(value) - set(schema)))
+
+
+def validate_dict(
+    value=_undefined,
+    schema=None, allow_extra=False,
+    required=True,
+):
+    """
+    Validates a dictionary, checking it against an optional schema.
+
+    The schema should be a dictionary, with keys corresponding to the expected
+    keys in `value`, but with the values replaced by functions which will be
+    called to with the corresponding value in the input.
+
+    A simple example:
+
+        validator = validate_dict(schema={
+            'id': validate_key(kind='Model'),
+            'count': validate_int(min=0),
+        })
+        validator({'id': self.key, 'count': self.count})
+
+    :param dict value:
+        The value to be validated.
+    :param dict schema:
+        The schema against which the value should be checked.
+    :param bool allow_extra:
+        Set to `True` to ignore extra keys.
+    :param bool required:
+        Whether the value can't be `None`. Defaults to True.
+    """
+    _validate_dict(schema, schema=None, required=False)
+    _validate_bool(allow_extra)
+    _validate_bool(required)
+
+    # Make a copy of the schema to make sure it won't be mutated under us.
+    if schema is not None:
+        schema = dict(schema)
+
+    def validate(value):
+        _validate_dict(
+            value,
+            schema=schema, allow_extra=allow_extra,
+            required=required,
+        )
+
+    if value is not _undefined:
+        validate(value)
+    else:
+        return validate
 
 
 def validate_enum(
