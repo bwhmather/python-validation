4ff7f008552cb2696c5e6b933a8e9df9e2cf9db9
Ben Mather <bwhmather@bwhmather.com>
2017-08-22 13:47:01 +0100

    Add python 3.6 to the list of supported versions

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     install_requires=[
         'six >= 1.10, < 2',
