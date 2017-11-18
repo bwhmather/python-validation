58eed0fc8252d75eb641c8700f885b520fca1a56
Ben Mather <bwhmather@bwhmather.com>
2017-11-18 00:31:14 +0000

    Add python 2.6 to list of supported versions

--- setup.py
+++ setup.py
@@ -29,6 +29,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
