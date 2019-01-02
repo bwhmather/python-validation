e3cba871701f55388aae063e3f5686f56efb2f7b
Hugo <hugovk@users.noreply.github.com>
2019-01-03 00:49:29 +0200

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     install_requires=[
         'six >= 1.10, < 2',
     ],
