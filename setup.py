22f479411f9ae27fc9cf9423c25bf9798b6a1790
Ben Mather <bwhmather@bwhmather.com>
2021-03-09 23:37:19 +0000

    Add python 3.8 and 3.9 to list of classifiers

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     install_requires=[
